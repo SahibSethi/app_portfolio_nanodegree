2299be5c71476ed32f0f8e9eb8a41a12c0e179c8
Sahib Sethi <sahib.sethi30@gmail.com>
2016-02-02 02:32:00 +0530

    App completed

--- app/src/main/java/ss/forkthecode/appportfolio/MainActivity.java
+++ app/src/main/java/ss/forkthecode/appportfolio/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void onSpotify(View view){
+        Toast.makeText(this, "This button will launch my spotify app", Toast.LENGTH_LONG).show();
+    }
+
+    public void onScores(View view){
+        Toast.makeText(this,"This button will launch my scores app", Toast.LENGTH_LONG).show();
+    }
+
+    public void onLibrary(View view){
+        Toast.makeText(this,"This button will launch my library app", Toast.LENGTH_LONG).show();
+    }
+
+    public void onBuild(View view){
+        Toast.makeText(this,"This button will launch my build app", Toast.LENGTH_LONG).show();
+    }
+
+    public void onReader(View view){
+        Toast.makeText(this,"This button will launch my reader app", Toast.LENGTH_LONG).show();
+    }
+
+    public void onCapstone(View view){
+        Toast.makeText(this,"This button will launch my capstone app", Toast.LENGTH_LONG).show();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
