a1a9675f99ee821516a10bc5b963d40ed5c04299
Sahib Sethi <sahib.sethi30@gmail.com>
2016-02-02 18:19:11 +0530

    Changes made according to reviews of submissions

--- app/src/main/java/ss/forkthecode/appportfolio/MainActivity.java
+++ app/src/main/java/ss/forkthecode/appportfolio/MainActivity.java
@@ -15,48 +15,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onAppClick(View view){
 
-    public void onSpotify(View view){
-        Toast.makeText(this, "This button will launch my spotify app", Toast.LENGTH_LONG).show();
-    }
+        switch(view.getId()){
+            case R.id.button1 :
+                Toast.makeText(this, "This button will launch my spotify app", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2 :
+                Toast.makeText(this,"This button will launch my scores app", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3 :
+                Toast.makeText(this,"This button will launch my library app", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4 :
+                Toast.makeText(this,"This button will launch my build app", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5 :
+                Toast.makeText(this,"This button will launch my reader app", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button6 :
+                Toast.makeText(this,"This button will launch my capstone app", Toast.LENGTH_SHORT).show();
+                break;
 
-    public void onScores(View view){
-        Toast.makeText(this,"This button will launch my scores app", Toast.LENGTH_LONG).show();
-    }
-
-    public void onLibrary(View view){
-        Toast.makeText(this,"This button will launch my library app", Toast.LENGTH_LONG).show();
-    }
-
-    public void onBuild(View view){
-        Toast.makeText(this,"This button will launch my build app", Toast.LENGTH_LONG).show();
-    }
-
-    public void onReader(View view){
-        Toast.makeText(this,"This button will launch my reader app", Toast.LENGTH_LONG).show();
-    }
-
-    public void onCapstone(View view){
-        Toast.makeText(this,"This button will launch my capstone app", Toast.LENGTH_LONG).show();
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
+
+
+
 }
